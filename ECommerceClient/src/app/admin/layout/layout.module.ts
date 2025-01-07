c6e84ba56331607f0197e838e040e7658502377d
IbrahimliCavid <ibrahimli.dev@gmail.com>
2025-01-07 18:45:51 +0100

    Updated

--- ECommerceClient/src/app/admin/layout/layout.module.ts
+++ ECommerceClient/src/app/admin/layout/layout.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ComponentsModule } from '../layout/components/components.module';
+
 
 
 
 @NgModule({
   declarations: [
     LayoutComponent,
-    FooterComponent,
-    HeaderComponent,
-    SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentsModule,
+], exports : [
+    LayoutComponent
   ]
 })
 export class LayoutModule { }
