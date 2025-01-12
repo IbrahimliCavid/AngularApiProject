e7f53f04cb5a35fe237e7331e02000c263927c2e
IbrahimliCavid <ibrahimli.dev@gmail.com>
2025-01-12 16:41:48 +0100

    Alertify Integration

--- ECommerceClient/src/app/admin/copmonents/dashboard/dashboard.component.ts
+++ ECommerceClient/src/app/admin/copmonents/dashboard/dashboard.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+declare var alertfy : any;
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+ 
+  constructor(private alertify : AlertifyService) {
+    
+  }
+  ngOnInit(): void {
+  }
+
+  m(){
+    this.alertify.message("Hello World", {
+      messageType : MessageType.Error,
+      position: Position.TopRight,
+      dismissOther: true
+    });
+  }
+
+  d(){
+    this.alertify.dismiss();
+  }
 
 }
