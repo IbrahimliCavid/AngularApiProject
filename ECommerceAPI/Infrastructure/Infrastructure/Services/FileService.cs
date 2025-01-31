be411fd773b38dd3b5cb676498749823b690ca77
IbrahimliCavid <ibrahimli.dev@gmail.com>
2025-01-31 20:50:03 +0100

    SEO-Friendly File Name Optimization

--- ECommerceAPI/Infrastructure/Infrastructure/Services/FileService.cs
+++ ECommerceAPI/Infrastructure/Infrastructure/Services/FileService.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Services;
+using Infrastructure.Operations;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using System;
@@ -23,7 +24,7 @@ namespace Infrastructure.Services
             try
             {
                 await using FileStream fileStream = new(path, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
-                await fileStream.CopyToAsync(fileStream);
+                await file.CopyToAsync(fileStream);
                 await fileStream.FlushAsync();
                 return true;
             }
@@ -35,9 +36,49 @@ namespace Infrastructure.Services
             }
         }
 
-        public Task<string> FileRenameAsync(string fileName)
+        async Task<string> FileRenameAsync(string path, string fileName, bool first = true)
         {
-            throw new NotImplementedException();
+        string newFileName =  await  Task.Run<string>(async () =>
+            {
+                string extension = Path.GetExtension(fileName);
+
+                string newFileName = string.Empty;
+
+                if (first)
+                {
+                    string oldFileName = Path.GetFileNameWithoutExtension(fileName);
+                    newFileName = $"{NameOperation.CharacterRegulatory(oldFileName)}{extension}";
+                }
+                else
+                { 
+                    newFileName = fileName;
+                    int indexNo1 = newFileName.IndexOf('-');
+                    if (indexNo1 == -1)
+                    {
+                        newFileName = $"{Path.GetFileNameWithoutExtension(fileName)}-2{extension}";
+                    }
+                    else
+                    {
+                        int indexNo2 = newFileName.IndexOf(".");
+                        string fileNo= newFileName.Substring(indexNo1, indexNo2 - indexNo1 - 1);
+                        int _fileNo = int.Parse(fileNo);
+                        _fileNo++;
+                        newFileName = newFileName.Remove(indexNo1, indexNo2 - indexNo1 - 1)
+                        .Insert(indexNo1, _fileNo.ToString());
+
+                    }
+                
+                }
+
+
+
+                if (File.Exists(Path.Combine(path, newFileName)))
+                   return await FileRenameAsync(path, newFileName, false);
+                else
+                    return newFileName;
+            });
+
+            return "";
         }
 
         public async Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files)
@@ -50,7 +91,7 @@ namespace Infrastructure.Services
             List<(string fileName, string path)> datas = new();
             List<bool> results = new();
             foreach (IFormFile file in files) { 
-               string newFileName = await FileRenameAsync(file.FileName);
+               string newFileName = await FileRenameAsync(uploadPath, file.FileName);
               bool result =  await CopyFileAsync(Path.Combine(uploadPath, newFileName), file);
                 datas.Add((newFileName, Path.Combine(uploadPath, newFileName)));
                 results.Add(result);    
