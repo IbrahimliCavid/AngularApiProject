ad583cc819158f0928163ec0b323e559668efae0
IbrahimliCavid <ibrahimli.dev@gmail.com>
2025-02-10 20:54:36 +0100

    Update README

--- ECommerceClient/src/app/dialogs/select-product-image-dialog/select-product-image-dialog.component.ts
+++ ECommerceClient/src/app/dialogs/select-product-image-dialog/select-product-image-dialog.component.ts
@@ -1,22 +1,33 @@
-import { Component, Inject, Output } from '@angular/core';
+import { Component, Inject, OnInit, Output } from '@angular/core';
 import { BaseDialog } from '../base/base-dialog';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FileUploadOptions } from 'src/app/services/common/file-upload/file-upload.component';
+import { ProductService } from 'src/app/services/common/models/product.service';
+import { ListProductImages } from 'src/app/contracts/list-product-images';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { SpinnerType } from 'src/app/base/base.component';
+import { DialogService } from 'src/app/services/common/dialog.service';
+import { DeleteDialogComponent, DeleteState } from '../delete-dialog/delete-dialog.component';
+declare var $ : any;
 
 @Component({
   selector: 'app-select-product-image-dialog',
   templateUrl: './select-product-image-dialog.component.html',
   styleUrls: ['./select-product-image-dialog.component.scss']
 })
-export class SelectProductImageDialogComponent extends BaseDialog<SelectProductImageDialogComponent> {
+export class SelectProductImageDialogComponent extends BaseDialog<SelectProductImageDialogComponent> implements OnInit {
 
  
   constructor(
     dialogref : MatDialogRef<SelectProductImageDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data : SelectProductImageState | string
-
+    @Inject(MAT_DIALOG_DATA) public data : SelectProductImageState | string,
+    private productService : ProductService,
+    private spinner : NgxSpinnerService,
+    private dialogService : DialogService
+     
   ) {
     super(dialogref); }
+
    @Output() options : Partial<FileUploadOptions> = {
       accept : ".jpg, .jpeg, .png, .gif",
       action : "upload",
@@ -24,6 +35,27 @@ export class SelectProductImageDialogComponent extends BaseDialog<SelectProductI
       explanation : "Upload product photo...",
       isAdminPage : true,
       queryString : `id=${this.data}`
+    }
+    images : ListProductImages[];
+    
+    async ngOnInit() {
+      this.spinner.show(SpinnerType.BallFussion)
+     this.images = await this.productService.readImages(this.data as string, ()=> this.spinner.hide(SpinnerType.BallFussion));
+    }
+    async deleteImage(imageId : string, event : any){
+     this.dialogService.openDialog({
+      coponentType: DeleteDialogComponent,
+      data : DeleteState.Yes,
+      afterClosed:async ()=>{
+        this.spinner.show(SpinnerType.BallFussion)
+        await this.productService.deleteImage(this.data as string, imageId, ()=>{
+         this.spinner.hide(SpinnerType.BallFussion);
+      var card = $(event.srcElement).parent().parent().parent();
+      card.fadeOut(500);
+        })
+      }
+     })
+  
     }
 }
 
--- README.md
+++ README.md
@@ -1,8 +1,32 @@
-# Layihənin məqsədi
-Bu layihənin məqsədi Angular texnologiyasını öyrənməyim üçündür. 
+Project Name: AngularApiProject
 
-## Texnologiyalar
-- Angular
-- C# (.NET Core)
-- PostgreSQL
+Description
+This project is designed to learn Angular and related technologies. It leverages several modern tools and practices to build a robust and scalable solution. The backend is built using C# (.NET Core) and PostgreSQL, following the Onion Architecture pattern, while the frontend is powered by Angular.
+
+Technologies Used
+
+Frontend:
+Angular
+Bootstrap 5
+Material
+Toastr
+Alertify
+Spinner
+
+Backend:
+C# (.NET Core)
+PostgreSQL
+CQRS Pattern
+Onion Architecture
+
+Storage:
+Azure Storage (for file saving)
+
+Features
+Onion Architecture: Ensures a well-structured and maintainable codebase by separating concerns.
+CQRS Pattern: The Command Query Responsibility Segregation (CQRS) pattern is applied to handle read and write operations separately.
+File Storage: Integrated Azure Storage for saving files.
+Material Design: UI components based on Material Design, providing a clean and modern interface.
+Notification Systems: Integrated Toastr and Alertify for displaying notifications.
+Spinner: To indicate loading processes.
 
