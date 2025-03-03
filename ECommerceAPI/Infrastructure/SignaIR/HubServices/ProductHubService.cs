17cfc48edab58a2839ad2a865bbe20a86c181019
IbrahimliCavid <ibrahimli.dev@gmail.com>
2025-03-03 18:17:29 +0400

    Initial setup for SignalR infrastructure integration (WIP)

---
+++ ECommerceAPI/Infrastructure/SignaIR/HubServices/ProductHubService.cs
@@ -0,0 +1,13 @@
+﻿using Application.Abstractions.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignaIR.HubServices
+{
+    public class ProductHubService : IProductHubService
+    {
+    }
+}
